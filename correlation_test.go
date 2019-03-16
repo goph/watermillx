01baccb17e0ba8a517b1492a17b589853431a758
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-03-16 11:01:29 +0100

    Fix correlation tests

--- correlation_test.go
+++ correlation_test.go
@@ -5,7 +5,9 @@ import (
 	"testing"
 	"time"
 
+	"github.com/ThreeDotsLabs/watermill"
 	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/ThreeDotsLabs/watermill/message/infrastructure/gochannel"
 	"github.com/ThreeDotsLabs/watermill/message/router/middleware"
 	"github.com/ThreeDotsLabs/watermill/message/subscriber"
 )
@@ -15,70 +17,70 @@ func TestCorrelationIDPublisherDecorator(t *testing.T) {
 		return "id", true
 	}
 
-	publisherStub := newPublisherStub()
+	pubsub := gochannel.NewGoChannel(gochannel.Config{}, watermill.NopLogger{})
 
-	publisher, err := CorrelationIDPublisherDecorator(fn)(publisherStub)
+	publisher, err := CorrelationIDPublisherDecorator(fn)(pubsub)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	const topic = "topic"
+
+	messages, err := pubsub.Subscribe(context.Background(), topic)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	msg := message.NewMessage("uuid", []byte{1, 2, 3})
 
-	const topic = "topic"
-
 	err = publisher.Publish(topic, msg)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if got, want := middleware.MessageCorrelationID(publisherStub.messages[topic][0]), "id"; got != want {
-		t.Errorf("message correlation ID does not match the expected\nactual:  %s\nexpected: %s", got, want)
+	received, all := subscriber.BulkRead(messages, 1, time.Second)
+	if !all {
+		t.Fatal("no message received")
+	}
+
+	if got, want := middleware.MessageCorrelationID(received[0]), "id"; got != want {
+		t.Errorf("message correlation ID does not match the expected\nactual:   %s\nexpected: %s", got, want)
 	}
 }
 
-type subscriberStub struct {
-	ch chan *message.Message
-}
-
-func (m subscriberStub) Subscribe(context.Context, string) (<-chan *message.Message, error) {
-	return m.ch, nil
-}
-
-func (m subscriberStub) Close() error {
-	close(m.ch)
-	return nil
-}
-
-func TestMessageTransformSubscriberDecorator_transparent(t *testing.T) {
+func TestCorrelationIDSubscriberDecorator(t *testing.T) {
 	var fn CorrelationIDInserter = func(msg *message.Message, id string) {
 		msg.Metadata.Set("mycid", id)
 	}
-	sub := subscriberStub{make(chan *message.Message)}
-	decorated, err := CorrelationIDSubscriberDecorator(fn)(sub)
+
+	pubsub := gochannel.NewGoChannel(gochannel.Config{}, watermill.NopLogger{})
+
+	sub, err := CorrelationIDSubscriberDecorator(fn)(pubsub)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	messages, err := decorated.Subscribe(context.Background(), "topic")
+	const topic = "topic"
+
+	messages, err := sub.Subscribe(context.Background(), topic)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	richMessage := message.NewMessage("uuid", []byte("serious payloads"))
-	richMessage.Metadata.Set("k1", "v1")
-	richMessage.Metadata.Set("k2", "v2")
-	middleware.SetCorrelationID("id", richMessage)
+	msg := message.NewMessage("uuid", []byte{1, 2, 3})
+	middleware.SetCorrelationID("id", msg)
 
-	go func() {
-		sub.ch <- richMessage
-	}()
+	err = pubsub.Publish(topic, msg)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	received, all := subscriber.BulkRead(messages, 1, time.Second)
 	if !all {
-		t.Fatal("expected to read all messages")
+		t.Fatal("no message received")
 	}
 
 	if got, want := received[0].Metadata.Get("mycid"), "id"; got != want {
-		t.Errorf("unexpected cid\nactual:  %s\nexpected: %s", got, want)
+		t.Errorf("message correlation ID does not match the expected\nactual:   %s\nexpected: %s", got, want)
 	}
 }
